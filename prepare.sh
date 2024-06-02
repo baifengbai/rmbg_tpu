ecb3f49c9bcb1a9a9bbfbd1aa1e3cdc83556807a
baifengbai <342741263@qq.com>
2024-06-02 21:44:22 +0800

    update tpu_perf for 0.4.8 sdk

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,9 @@ set -e
 echo "Installing Python dependencies..."
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
+## 4.8驱动安装1.2.21  5.0的安装1.2.35
+pip3 install tpu_perf-1.2.21-py3-none-manylinux2014_aarch64.whl
+
 # 下载模型文件
 echo "Downloading model files..."
 python3 -m dfss --url=open@sophgo.com:/aigc/models.zip
